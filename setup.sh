fdbe0a8e9ced6b5e01d3df1b626f980a5353f09c
Jeremy Ellis <keyfreemusic@gmail.com>
2016-06-08 13:52:04 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -55,10 +55,10 @@ echo "Now trying Bazel"
 #chmod a+x bazel
 
 
-sudo apt-get install software-properties-common
+sudo apt-get  -y install software-properties-common
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer pkg-config zip g++ zlib1g-dev unzip
+sudo apt-get -y install oracle-java8-installer pkg-config zip g++ zlib1g-dev unzip
 
 mkdir /home/ubuntu/workspace/bazel
 
